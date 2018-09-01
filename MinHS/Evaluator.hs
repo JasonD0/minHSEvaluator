db662fea2d98d1c7d4d7bdd76b601c2625b01011
Jason Do <Jason.Do1@hotmail.com>
2018-09-01 18:42:23 +1000

    implemented evaluation for constants/booleans, primitive op

--- MinHS/Evaluator.hs
+++ MinHS/Evaluator.hs
@@ -3,6 +3,7 @@ import qualified MinHS.Env as E
 import MinHS.Syntax
 import MinHS.Pretty
 import qualified Text.PrettyPrint.ANSI.Leijen as PP
+import qualified Env
 
 type VEnv = E.Env Value
 
@@ -26,4 +27,33 @@ evaluate bs = evalE E.empty (Let bs (Var "main"))
 
 
 evalE :: VEnv -> Exp -> Value
-evalE = error "implement me!"
+
+-- Constants and Boolean Constructors
+evalE env (Num value) = I value
+evalE env (Con "True") = B True
+evalE env (Con "False") = B False
+
+-- Primitive Operations
+evalE env (App (App (Prim Op e1)) e2) = 
+  let I val1 = evalE env e1 
+      I val2 = evalE env e2
+  in case Op of
+    (Add)  -> I (val1 + val2)
+    (Sub)  -> I (val1 - val2)
+    (Mul)  -> I (val1 * val2)
+    (Quot) -> case val2 of
+      (0) -> error "Division by 0"
+       _  -> I (quot val1 val2) 
+    (Rem)  -> case val2 of 
+      (0) -> error "Division by 0"
+       _  -> I (rem val1 val2)
+    (Gt)   -> B (val1 > val2)
+    (Ge)   -> B (val1 >= val2)
+    (Lt)   -> B (val1 < val2)
+    (Le)   -> B (val1 <= val2)
+    (Eq)   -> B (val1 == val2)
+    (Ne)   -> B (val1 /= val2)
+evalE env (App (Prim Neg e1)) =
+  let I val = evalE env e1
+  in I (negate(val))
+
